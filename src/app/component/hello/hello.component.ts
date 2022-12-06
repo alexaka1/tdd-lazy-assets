913b6daf9b25483f65f0fc6e0cc51afbec56ddd0
Martossy Alex <github@mail.martossy.hu>
2022-12-06 10:04:25 +0100

    eslint fixes

--- src/app/component/hello/hello.component.ts
+++ src/app/component/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   template: `
@@ -7,8 +7,4 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: [],
 })
-export class HelloComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class HelloComponent {}
--- src/app/modules/admin/components/editor/editor.component.ts
+++ src/app/modules/admin/components/editor/editor.component.ts
@@ -1,46 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SummernoteOptions } from 'ngx-summernote/lib/summernote-options';
 
 @Component({
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent implements OnInit {
-  config: SummernoteOptions = {
-    placeholder: '',
-    tabsize: 2,
-    // height: '200px',
-    // uploadImagePath: '/api/upload',
-    toolbar: [
-      ['misc', ['codeview', 'undo', 'redo']],
-      ['style', ['bold', 'italic', 'underline', 'clear']],
-      [
-        'font',
+export class EditorComponent {
+  config: SummernoteOptions;
+  constructor() {
+    this.config = {
+      placeholder: '',
+      tabsize: 2,
+      // height: '200px',
+      // uploadImagePath: '/api/upload',
+      toolbar: [
+        ['misc', ['codeview', 'undo', 'redo']],
+        ['style', ['bold', 'italic', 'underline', 'clear']],
         [
-          'bold',
-          'italic',
-          'underline',
-          'strikethrough',
-          'superscript',
-          'subscript',
-          'clear',
+          'font',
+          [
+            'bold',
+            'italic',
+            'underline',
+            'strikethrough',
+            'superscript',
+            'subscript',
+            'clear',
+          ],
         ],
+        ['fontsize', ['fontname', 'fontsize', 'color']],
+        ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
+        ['insert', ['table', 'picture', 'link', 'video', 'hr']],
       ],
-      ['fontsize', ['fontname', 'fontsize', 'color']],
-      ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
-      ['insert', ['table', 'picture', 'link', 'video', 'hr']],
-    ],
-    fontNames: [
-      'Helvetica',
-      'Arial',
-      'Arial Black',
-      'Comic Sans MS',
-      'Courier New',
-      'Roboto',
-      'Times',
-    ],
-  };
-  constructor() {}
-
-  ngOnInit(): void {}
+      fontNames: [
+        'Helvetica',
+        'Arial',
+        'Arial Black',
+        'Comic Sans MS',
+        'Courier New',
+        'Roboto',
+        'Times',
+      ],
+    };
+  }
 }
